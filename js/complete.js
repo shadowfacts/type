27e7357e524bafd64b9a3b5f03b8507bd1818c51
Shadowfacts <me@shadowfacts.net>
2016-10-02 11:06:53 -0400

    Add repo link to complete page

--- js/complete.js
+++ js/complete.js
@@ -1,7 +1,10 @@
 let hash = window.location.hash.substring(1);
 
 let hashBits = hash.split("/");
-let repo = hashBits.slice(0, 2).join("/");
+let repo = hashBits.slice(0, 3).join("/");
+let repoName = hashBits.slice(0, 2).join("/");
 let file = hashBits[hashBits.length - 1];
 
-$("#congrats").html(`Congratulations, you've completed <code>${file}</code> in <code>${repo}</code>!`);
+$("#congrats").html(`Congratulations, you've completed <code>${file}</code> in <code>${repoName}</code>!`);
+
+$("#repo").attr("href", `/repo.html#${repo}`);
