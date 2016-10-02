3650355593480211d90296c0990bb0c0238e0689
Shadowfacts <me@shadowfacts.net>
2016-10-02 10:34:57 -0400

    Show previous repo/file lists

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+let prevRepoList = $("#prev-repos");
+
+localforage.keys()
+	.then((keys) => {
+		return keys.filter((el) => {
+			return el != "theme";
+		});
+	})
+	.then((repos) => {
+		repos.forEach((el) => {
+			let li = $("<li></li>");
+			let a = $("<a></a>");
+			a.attr("href", `/repo.html#${el}`);
+			let div = $("<div></div>");
+			div.text(el);
+			a.append(div);
+			li.append(a);
+			prevRepoList.append(li);
+		});
+	})
+	.catch((e) => {
+		throw e;
+	});
+
 $("#form").submit((event) => {
 	event.preventDefault();
 
---
+++ js/repo.js
@@ -0,0 +1,25 @@
+let repo = window.location.hash.substring(1);
+
+localforage.getItem(repo)
+	.then((val) => {
+		for (var f in val) {
+			let li = $("<li></li>");
+			let a = $("<a></a>");
+			a.attr("href", `/type.html#${repo}/${f}`);
+			let div = $("<div></div>");
+			div.text(f);
+			a.append(div);
+			li.append(a);
+			$("#prev-files").append(li);
+		}
+	})
+	.catch((e) => {
+		throw e;
+	});
+
+$("#form").submit((event) => {
+	event.preventDefault();
+
+	let file = $("#file").val();
+	window.location.href = `/type.html#${repo}/${file}`;
+});
